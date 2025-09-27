e4f0c025bcc4f6a4d125e8debcf7f3dfe091d19a
is13mael <ismaelparramoussaif@gmail.com>
2025-09-28 00:15:07 +0200

    Added Ejercicio6.java

---
+++ src/Ejercicio6.java
@@ -0,0 +1,13 @@
+public class Ejercicio6 {
+    public static void main(String[] args){
+        System.out.println("Área de un triángulo");
+        System.out.println("─────────────────────");
+        System.out.print("Por favor, introduzca la longitud de la base (cm): ");
+        double base = Double.parseDouble(System.console().readLine());
+        System.out.print("Introduzca la altura (cm): ");
+        double altura = Double.parseDouble(System.console().readLine());
+        System.out.println("El área del triángulo es ");
+        System.out.println((base * altura)/2 + " cm²");
+
+    }
+}
