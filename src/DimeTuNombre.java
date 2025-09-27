e560f6b305cdba56f7770f02650f99a82a30eb52
is13mael <ismaelparramoussaif@gmail.com>
2025-09-27 21:13:08 +0200

    Added DimeTuNombre.java

---
+++ src/DimeTuNombre.java
@@ -0,0 +1,12 @@
+/**
+ * Lecturas de datos desde teclado.
+ */
+public class DimeTuNombre {
+    public static void main(String[] args){
+        String nombre;
+        System.out.print("Por favor, dime como te llamas: ");
+        nombre = System.console().readLine();
+        System.out.println("Hola " + nombre + ", ¡encantado de conocerte!");
+
+    }
+}
